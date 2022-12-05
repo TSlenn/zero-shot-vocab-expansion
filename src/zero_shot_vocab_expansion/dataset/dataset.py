fe2c51633bd34bc8a6b7cef9da96993adea1823f
Terry Slenn <terry.slenn@gmail.com>
2022-12-04 23:26:28 -0500

    expand dataset init options and testing

--- src/zero_shot_vocab_expansion/dataset/dataset.py
+++ src/zero_shot_vocab_expansion/dataset/dataset.py
@@ -1,14 +1,60 @@
 from torch.utils.data import Dataset
 import random
+from transformers import AutoModel, AutoTokenizer
+from transformers.modeling_utils import PreTrainedModel
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from sentence_transformers import SentenceTransformer
+from typing import Union
 from .utils import get_definitions
 
 
 class VocabDataset(Dataset):
-    def __init__(self, model, tokenizer):
-        self.embeddings = model.embeddings.word_embeddings.weight.detach()
+    """Dataset of word definitions and corresponding embeddings."""
+    def __init__(
+        self,
+        model: Union[str, PreTrainedModel, SentenceTransformer],
+        tokenizer: PreTrainedTokenizerBase = None,
+        definitions: dict = {}
+    ):
+        """Finds model vocab and corresponding word definitions.
+
+        VocabDataset extracts vocabulary from a transformers tokenizer or
+        SentenceTransformer, and matches them with a target embedding from
+        a transformers model or SentenceTransformer backbone model. Word
+        definitions are discovered using nltk wordnet synsets, or user provided
+        definitions.
+
+        Args:
+            model (str, PreTrainedModel, SentenceTransformer): Pretrained
+                model, model name, or path to a local model.
+            tokenizer (PreTrainedTokenizerBase): Optional. Tokenizer for model.
+                If model is provided as str or SentenceTransformer, the
+                corresponding tokenizer will be loaded automatically.
+            definitions (dict): Dictionary of user provided word definitions.
+                These are prioritized over wordnet definitions.
+                {"word": ["definition1", "definition2"]}
+        """
+        # get transformers model, tokenizer
+        if isinstance(model, str):
+            mod = AutoModel.from_pretrained(model)
+            if tokenizer is None:
+                tokenizer = AutoTokenizer.from_pretrained(model)
+        elif isinstance(model, PreTrainedModel):
+            mod = model
+            msg = "tokenizer must be provided with transformer model"
+            assert tokenizer is not None, msg
+        elif isinstance(model, SentenceTransformer):
+            tokenizer = model.tokenizer
+            mod = model._first_module().auto_model
+
+        # get word definitions and target embeddings
+        self.embeddings = mod.embeddings.word_embeddings.weight.detach()
         self.token_map = tokenizer.vocab
         words = list(self.token_map.keys())
-        definitions = [get_definitions(word) for word in words]
+        definitions = [
+            definitions[word] if word in definitions else get_definitions(word)
+            for word in words
+        ]
         self.definitions = {
             word: defs for word, defs in zip(words, definitions)
             if len(defs) > 0
--- src/zero_shot_vocab_expansion/dataset/test/test_dataset.py
+++ src/zero_shot_vocab_expansion/dataset/test/test_dataset.py
@@ -1,14 +1,39 @@
 from zero_shot_vocab_expansion.dataset import VocabDataset
 from transformers import AutoModel, AutoTokenizer
-
-MODEL = AutoModel.from_pretrained("bert-base-uncased")
-TOKENIZER = AutoTokenizer.from_pretrained("bert-base-uncased")
+from sentence_transformers import SentenceTransformer
 
 
-def test_VocabDataset():
-    ds = VocabDataset(MODEL, TOKENIZER)
+def test_VocabDataset_transformers():
+    model = AutoModel.from_pretrained("bert-base-uncased")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    ds = VocabDataset(model, tokenizer)
     assert len(ds) > 0
     output = ds[0]
     assert "word" in output
     assert "definition" in output
     assert "target" in output
+
+
+def test_VocabDataset_str():
+    ds = VocabDataset("bert-base-uncased")
+    assert len(ds) > 0
+    output = ds[0]
+    assert "word" in output
+    assert "definition" in output
+    assert "target" in output
+
+
+def test_VocabDataset_ST():
+    model = SentenceTransformer("bert-base-uncased")
+    ds = VocabDataset(model)
+    assert len(ds) > 0
+    output = ds[0]
+    assert "word" in output
+    assert "definition" in output
+    assert "target" in output
+
+
+def test_VocabDataset_custom_defs():
+    defs = {"test": ["hello world"]}
+    ds = VocabDataset("bert-base-uncased", definitions=defs)
+    assert ds.definitions["test"][0] == "hello world"
